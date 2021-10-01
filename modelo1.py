5defe22c544ada8359584f169e41e4b758f5f488
FelixErnesto21 <felix.astudillo21@gmail.com>
2021-10-01 17:16:26 -0400

    creando el fichero nomina.txt

--- modelo1.py
+++ modelo1.py
@@ -5,21 +5,15 @@ bono_general=float(input("Ingrese el valor del Bono General:"))
 bono_eficiencia=float(input("Ingrese el valor del Bono de Eficiencia:"))
 
 #2: Leer archivo
-with open("Archivo nomina.txt","r",encoding = "utf-8") as nomina:
-    nomina = nomina.readlines()
-
-cedula=[]
-ingreso=[]
-empleado=[]
-sueldo=[]
-horas=[]
-
-for i in nomina: 
-    print(i)
-    cedula.insert(0,i)
-
 print("")
-print(cedula)
-    
+lista=[]
+with open("creando el fichero nomina.txt","r",encoding = "utf-8") as nomina:
+    nomina = nomina.readlines()
+    for lineas in nomina:
+        lista.append(lineas.split("\n"))
 
-    
+lista = (lista[1:]) 
+for j in lista:
+    print(j)
+
+input()
